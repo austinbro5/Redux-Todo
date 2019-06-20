f6fdd9823e42127fc4209560db59c03df2c1c90b
austin breaux <47146701+austinbro5@users.noreply.github.com>
2019-06-19 22:22:24 -0500

    fixed bugs and styling

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,19 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux"; //gives us access to createStore function from redux
-import { Provider } from "react-redux"; //gives us access to Provider Component, which makes our app aware of store
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import App from './App';
 
-import reducer from "./reducers";
+const store = createStore(rootReducer);
 
-import "./index.css";
-import TodoApp from "./components/TodoApp";
 
-const store = createStore(reducer); //createStore function receives reducer responsible for updating the store, along with any initial state
-
-//Provider is where store lives and it makes our app aware of store, by wrapping our component inside of it
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp />
+    <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
