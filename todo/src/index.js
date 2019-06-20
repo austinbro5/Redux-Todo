f0bd6b857406dcd2b88cd12ef7cf21feb3a27fc5
austin breaux <47146701+austinbro5@users.noreply.github.com>
2019-06-19 20:42:43 -0500

    added index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux"; //gives us access to createStore function from redux
+import { Provider } from "react-redux"; //gives us access to Provider Component, which makes our app aware of store
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from "./reducers";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import "./index.css";
+import TodoApp from "./components/TodoApp";
+
+const store = createStore(reducer); //createStore function receives reducer responsible for updating the store, along with any initial state
+
+//Provider is where store lives and it makes our app aware of store, by wrapping our component inside of it
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById("root")
+);
